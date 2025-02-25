921bf697d292aee0a784f47a296820cd7e5481e5
Sai kowshik Pusurla <161359939+Saikowshik2468@users.noreply.github.com>
2025-02-25 11:32:51 +0530

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  // Simulate login (replace with your authentication logic)
+  window.location.href = "dashboard.html";
+});
