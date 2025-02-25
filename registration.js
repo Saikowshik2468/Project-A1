4b66fe004b58720b75876b2a118c023ccb44a38a
Sai kowshik Pusurla <161359939+Saikowshik2468@users.noreply.github.com>
2025-02-25 11:34:22 +0530

    Create registration.js

---
+++ registration.js
@@ -0,0 +1,6 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  // Simulate registration (replace with your registration logic)
+  alert("Registration Successful!");
+  window.location.href = "index.html";
+});
